5e226179f351aef91e23ded285a2f865280e78ab
oscardiaz1331 <oscardiaz1331@gmail.com>
2024-06-07 15:50:57 +0200

    generate postfix completed

--- JavaProjectAutomaticMarkerForGroupsOf2/spreadsheetMarkerForStudents/src/main/java/edu/upc/etsetb/arqsoft/spreadsheet/domainmodel/Operator.java
+++ JavaProjectAutomaticMarkerForGroupsOf2/spreadsheetMarkerForStudents/src/main/java/edu/upc/etsetb/arqsoft/spreadsheet/domainmodel/Operator.java
@@ -10,12 +10,57 @@ package edu.upc.etsetb.arqsoft.spreadsheet.domainmodel;
  */
 public class Operator implements FormulaComponent{
     private String operator;
+    private int priority;
+    private OperatorType type;
+    
+    public enum OperatorType {
+        ADD,
+        SUB,
+        MULT,
+        DIV,
+        OPEN_PAREN, 
+        CLOSE_PAREN, 
+    }
     
     public Operator(String operator){
         this.operator = operator;
+        switch(operator){
+            case "+":
+                this.type = OperatorType.ADD;
+                this.priority = 1;
+                break;
+            case "-":
+                this.type = OperatorType.SUB;
+                this.priority = 1;
+                break;
+            case "*":
+                this.type = OperatorType.MULT;
+                this.priority = 2;
+                break;
+            case "/":
+                this.type = OperatorType.DIV;
+                this.priority = 2;
+                break;
+            case ")":
+                this.type = OperatorType.CLOSE_PAREN;
+                this.priority = 3;
+                break;
+            case "(":
+                this.type = OperatorType.OPEN_PAREN;
+                this.priority = 0;
+                break;
+        }
     }
     
     public String getOperator(){
         return this.operator;
+    }
+    
+    public int getPriority(){
+        return this.priority;
+    }
+    
+    public OperatorType getType(){
+        return this.type;
     }
 }
--- JavaProjectAutomaticMarkerForGroupsOf2/spreadsheetMarkerForStudents/src/main/java/edu/upc/etsetb/arqsoft/spreadsheet/usecases/project/PostfixExpression.java
+++ JavaProjectAutomaticMarkerForGroupsOf2/spreadsheetMarkerForStudents/src/main/java/edu/upc/etsetb/arqsoft/spreadsheet/usecases/project/PostfixExpression.java
@@ -5,7 +5,9 @@
 package edu.upc.etsetb.arqsoft.spreadsheet.usecases.project;
 
 import edu.upc.etsetb.arqsoft.spreadsheet.domainmodel.FormulaComponent;
+import edu.upc.etsetb.arqsoft.spreadsheet.domainmodel.Operator;
 import edu.upc.etsetb.arqsoft.spreadsheet.exceptions.MathErrorException;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -14,18 +16,47 @@ import java.util.List;
  */
 public class PostfixExpression {
     List<FormulaComponent> components;
+    LinkedList<Operator> stack;
     
     public PostfixExpression(List<FormulaComponent> components){
         this.components = components;
     }
     
-    //public List<FormulaComponent> generate(List<FormulaComponent> components){
-        
-    //}
-    //public Number evaluate() throws MathErrorException {
-        //TODO
-
-    //}
+    public void generate(){
+        this.stack = new LinkedList<>();
+        for(FormulaComponent comp : components){
+            if(comp instanceof Operator){
+                Operator op =(Operator) comp;
+                if(op.getType() == Operator.OperatorType.OPEN_PAREN){
+                    stack.add(op);
+                }
+                else if(op.getType() == Operator.OperatorType.CLOSE_PAREN){
+                    while(stack.getLast().getType() != Operator.OperatorType.OPEN_PAREN){
+                        components.add(stack.removeLast());
+                        stack.add(op);
+                    }
+                    stack.removeLast();
+                }
+                else if(stack.isEmpty() || stack.getLast().getPriority() > op.getPriority()){
+                    stack.add(op);
+                }
+                else{  
+                    components.add(stack.removeLast());
+                    stack.add(op);
+                }
+            }
+            else{
+                components.add(comp);
+            }
+        }
+        while (!stack.isEmpty()) {
+            components.add(stack.removeLast());
+        }
+    }
+//    public Number evaluate() throws MathErrorException {
+//        this.stack = new LinkedList<>();
+//
+//    }
     
     
 }
